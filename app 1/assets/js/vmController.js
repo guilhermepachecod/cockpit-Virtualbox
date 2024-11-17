0021f3c5b3c25b82a6583c5d5e1aca2554296728
guilhermepachecod <42876645+guilhermepachecod@users.noreply.github.com>
2024-11-17 17:21:02 -0300

    bugfix

--- app 1/assets/js/vmController.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('startStopVM').addEventListener('click', () => {
-  // Lógica para start/stop da VM
-  console.log("Iniciar/Parar VM");
-});
-
-document.getElementById('newVM').addEventListener('click', () => {
-  // Lógica para criar nova VM
-  console.log("Nova VM");
-});
